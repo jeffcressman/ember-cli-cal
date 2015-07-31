83431dbbcae700e34f98a29f65a93fa4c6d01bb0
Jeff Cressman <jeff.cressman@gmail.com>
2015-07-31 12:11:17 +0930

    Updated moment and lodash

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# 0.1.6
+
+* Updated moment to 2.10.3
+* Updated lodash to 3.7.0
--- index.js
+++ index.js
@@ -2,9 +2,9 @@ module.exports = {
     name: 'ember-cli-cal',
     included: function(app) {
         this._super.included(app);
-        
+
         app.import(app.bowerDirectory + '/moment/moment.js');
-        app.import(app.bowerDirectory + '/lodash/dist/lodash.js');
+        app.import(app.bowerDirectory + '/lodash/lodash.js');
         app.import('vendor/ember-cli-cal.css');
     }
 };
